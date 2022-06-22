2363f9ac86140a8255611e5fe37f3ccff1bfa001
Vacip Derici <dericivd@gmail.com>
2022-06-22 18:03:11 +0300

    Write First Test for Android App

---
+++ src/test/java/FirstAndroidTest.java
@@ -0,0 +1,40 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FirstAndroidTest {
+
+    AppiumDriver driver;
+
+
+  @BeforeTest
+  public void setUp() throws MalformedURLException {
+      DesiredCapabilities caps =new DesiredCapabilities();
+      caps.setCapability("platformName","Android");
+      caps.setCapability("automationName","UiAutomator2");
+      caps.setCapability("deviceName","HUAWEI P40 Lite");
+      caps.setCapability("app",System.getProperty("user.dir")+"/apps/ApiDemos-debug.apk");
+
+      driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"),caps);
+
+  }
+    @Test
+    public  void click_App_Button(){
+      driver.findElementsByAccessibilityId("app");
+    }
+
+    @AfterTest
+    public void TearDown(){
+      if (null != driver){
+          driver.quit();
+      }
+    }
+
+
+ }
