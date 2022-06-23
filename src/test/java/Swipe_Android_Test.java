8eea5a33ac1b5c7e0b97d06a0c02c2873c0a7583
Vacip Derici <dericivd@gmail.com>
2022-06-23 22:00:24 +0300

    swipe android test

--- src/test/java/Swipe_Android_Test.java
+++ src/test/java/Swipe_Android_Test.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class Swipe_Android_Test {
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.AndroidTouchAction;
+import io.appium.java_client.touch.offset.ElementOption;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Swipe_Android_Test {
+
+    public AndroidDriver driver;
+    public AndroidTouchAction actions;
+
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities caps =new DesiredCapabilities();
+        caps.setCapability("platformName","Android");
+        caps.setCapability("automationName","UiAutomator2");
+        caps.setCapability("deviceName","HUAWEI P40 Lite");
+        caps.setCapability("appPackage","io.appium.android.apis");
+        caps.setCapability("appActivity","io.appium.android.apis.ApiDemos");
+
+
+        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"),caps);
+
+    }
+
+    @Test
+    public void Swipe_Test(){
+        AndroidElement views = (AndroidElement) driver.findElementByAccessibilityId("Views");
+
+        actions = new AndroidTouchAction(driver);
+        actions.tap(ElementOption.element(views)).perform();  //click views
+
+        AndroidElement gallery =
+                (AndroidElement) driver.findElementByAccessibilityId("Gallery");
+        actions.tap(ElementOption.element(gallery)).perform(); // Click Gallery
+
+        AndroidElement photo =(AndroidElement)  driver.findElementByAccessibilityId("1. Photos");
+        actions.tap(ElementOption.element(photo));
+
+
+
+
+        // AndroidElement pic1 = (AndroidElement) driver.findElements(By.className("android.widget.ImageView")).get(0);
+
+       // actions.press(ElementOption.element((pic1))).waitAction()
+        //        .moveTo(PointOption.point(-20,210)).release().perform();
+
+
 }
+    @AfterTest
+    public void TearDown(){
+        if (null != driver){
+            driver.quit();
+        }
+    }
+}
+
