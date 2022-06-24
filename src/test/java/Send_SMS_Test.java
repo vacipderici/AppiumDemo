2dba850412e02565d89001dfc87abd8b4ac293a6
Vacip Derici <dericivd@gmail.com>
2022-06-24 20:46:47 +0300

    send sms message test

---
+++ src/test/java/Send_SMS_Test.java
@@ -0,0 +1,42 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidTouchAction;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Send_SMS_Test {
+
+    public AndroidDriver driver;
+    public AndroidTouchAction actions;
+
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities caps =new DesiredCapabilities();
+        caps.setCapability("platformName","Android");
+        caps.setCapability("automationName","UiAutomator2");
+        caps.setCapability("deviceName","HUAWEI P40 Lite");
+        caps.setCapability("appPackage","com.android.mms");
+        caps.setCapability("appActivity","com.huawei.mms.ui.ConversationList");
+
+
+        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"),caps);
+
+    }
+
+    @Test
+    public void send_SMS(){
+
+    }
+
+    @AfterTest
+    public void TearDown(){
+        if (null != driver){
+            driver.quit();
+        }
+    }
+
+}
